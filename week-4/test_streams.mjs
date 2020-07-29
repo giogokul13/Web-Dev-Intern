38eea31290852cdfb0b908e9700c11e0232a140a
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-07-29 22:29:42 +0530

    Create test_streams.mjs
    read and write file content

---
+++ week-4/test_streams.mjs
@@ -0,0 +1,15 @@
+import { createReadStream, createWriteStream } from "fs";
+
+var RS = createReadStream("sample2.txt",{ encoding : "utf8" });
+var Ws = createWriteStream("sample2.txt");
+console.log("the file is being read ......");
+
+console.log(" the file contents are.....");
+
+RS.on("data",function(buffer){
+    console.log(buffer);
+});
+
+Ws.write(" the data is written here overlapped");
+console.log("the file content is replaced !!!..");
+
