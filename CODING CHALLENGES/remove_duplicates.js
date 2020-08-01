18d9141d7bc8ed61b01818ca3d73d4cddecfe2cd
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-01 21:14:28 +0530

    Create remove_duplicates.js

---
+++ CODING CHALLENGES/remove_duplicates.js
@@ -0,0 +1,10 @@
+// function duplicate(string)
+// {    var str = string;
+//      var newstr = "";
+//      var str1  =[...string];
+     
+//      console.log(" The Original String is : " + str);
+//      console.log(" The Original String is : " + str1.sort());
+    
+// }
+// duplicate("affdabbc");
