a1a7dc72280e9884a1eddc3500c51e79da4eca61
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-03 14:24:47 +0530

    Create union operator.js
    union operator

---
+++ week-5/union operator.js
@@ -0,0 +1,15 @@
+function concat_add(val1, val2) {
+    if (typeof val1 === "string" && typeof val2 === "string") {
+        return val1 + val2;
+    }
+    else if (typeof val1 === "number" && typeof val2 === "number") {
+        return val1 + val2;
+    }
+    return null;
+}
+var a = concat_add("hi ", "this is string ");
+console.log(a);
+console.log(typeof a);
+var b = concat_add(1212, 2323);
+console.log(b);
+console.log(typeof b);
