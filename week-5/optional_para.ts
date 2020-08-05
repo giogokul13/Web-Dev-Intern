75227facdb536529cfca8763b4a84ee5c8319d67
giogokul13 <63816836+giogokul13@users.noreply.github.com>
2020-08-05 18:13:38 +0530

    Create optional_para.ts

---
+++ week-5/optional_para.ts
@@ -0,0 +1,7 @@
+function optional_para(x:number , y:number,z?:number){
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+optional_para(1,2,3); // with 3 args
+optional_para(1,2); // with 2 args 
